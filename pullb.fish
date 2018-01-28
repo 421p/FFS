b474827beb871ce6ce21787cd4867e3b9d9b2657
Andrii Bakal <andrew.bakal@gmail.com>
2018-01-28 03:23:58 +0200

    typo/2

--- pullb.fish
+++ pullb.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
-function pullb --description 'alias pullb=git pullb origin (git rev-parse --abbrev-ref HEAD)'
+function pullb --description 'alias pullb=git pull origin (git rev-parse --abbrev-ref HEAD)'
 	git pull origin (git rev-parse --abbrev-ref HEAD) $argv;
 end
